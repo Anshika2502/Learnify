150aae277bd6cf06b8e0decd5984e051b4b9e99a
Anshika2502 <bindraa2502@gmail.com>
2023-08-19 16:28:04 +0530

    Course Permission

--- content/views.py
+++ content/views.py
@@ -2,6 +2,8 @@ from typing import Any, Dict
 from django.shortcuts import render , get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Course , Video
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import CoursePermissionMixin
 
 # Create your views here.
 class CourseListView(ListView):
@@ -14,7 +16,7 @@ class CourseDetailView(DetailView):
     model = Course
     template_name = "content/course_detail.html"
 
-class VideoDetailView(DetailView):
+class VideoDetailView(LoginRequiredMixin, CoursePermissionMixin, DetailView):
     model = Video
     template_name = "content/video_detail.html"
 
@@ -22,4 +24,10 @@ class VideoDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         course = get_object_or_404(Course , slug=self.kwargs["course_slug"])
         context['course'] = course
+
+        subscription = self.request.user.student.subscription
+        pricing_tier = subscription.pricing
+
+        pricing_tier in course.pricing_tiers.all()
+
         return context
